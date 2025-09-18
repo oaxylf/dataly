10f62cd075fcbfe31e2da1c0ccd454c73338c84a
oaxylf.dev <rayyt2551@gmail.com>
2025-09-18 13:26:41 +0000

    Implement EsLint and Prettier for better code-style view

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello from dataly API!")
-})
+  res
+    .status(200)
+    .send('Hello from dataly API! This is a successful result from express'); // This will print this message as if it is successful in the web.
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import app from './app.js';
+import app from './app.js'; // import the module of app from ./app.js
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // This does saves the port to 3000
 
 app.listen(PORT, () => {
-    console.log(`App is listening on http://localhost:${PORT}...`);
-})
+  console.log(`App is listening on http://localhost:${PORT}...`);
+});
